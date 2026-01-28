fdf3653d8347498c848e251dea2a1155c1fd476b
Deepak Erothi <deepakerothi7@gmail.com>
2026-01-28 18:57:09 +0530

    Learned how to find a prime number

--- employee_management_system/src/components/Auth/Login.jsx
+++ employee_management_system/src/components/Auth/Login.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(" Email: ", email);
+    console.log(" Password: ", password);
+  }
+    
   return (
     <div className='flex items-center justify-center h-screen'>
       <div className='border-2 border-red-600 border-opacity-50 rounded-lg shadow-lg py-18 px-3'>
-        <form className='flex flex-col items-center justify-center p-7 gap-5'>
+        <form 
+        onSubmit={(e)=>{
+          submitHandler(e)
+        }}
+        className='flex flex-col items-center justify-center p-7 gap-5'>
           <h1 className='text-2xl font-bold text-2xl font-bold mt-[-10px]'>Login Page</h1>
-          <input className=' border-2 border-red-600 py-3 px-5 rounded-full bg-transparent text-white outline-none placeholder-gray-400 focus:placeholder-gray-600 focus:border-red-500' type="email" placeholder='Enter your email' />
-          <input className=' border-2 border-red-600 py-3 px-5 rounded-full bg-transparent text-white outline-none placeholder-gray-400 focus:placeholder-gray-600 focus:border-red-500' type="password" placeholder='Enter your password' />
+          <input
+          value={email}
+           onChange={(e)=>{
+            setEmail(e.target.value)
+           }}
+           required 
+           className=' border-2 border-red-600 py-3 px-5 rounded-full bg-transparent text-white outline-none placeholder-gray-400 focus:placeholder-gray-600 focus:border-red-500' type="email" placeholder='Enter your email' />
+          <input 
+          value={password}
+           onChange={(e)=>{
+            setPassword(e.target.value)
+           }}
+          required 
+          className=' border-2 border-red-600 py-3 px-5 rounded-full bg-transparent text-white outline-none placeholder-gray-400 focus:placeholder-gray-600 focus:border-red-500' type="password" placeholder='Enter your password' />
           <button className='bg-red-600 text-white border-2 border-red-600 py-3 px-5 rounded-full bg-transparent text-white outline-none placeholder-gray-400 focus:placeholder-gray-600 focus:border-red-500'>Login</button>
         </form>
       </div>
     </div>
-  )
+  )   
 }
 
 export default Login
