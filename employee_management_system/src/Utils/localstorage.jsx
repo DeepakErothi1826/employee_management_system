acbb698f316bc932f9ae3ed7dc6f73595659c89d
Deepak Erothi <deepakerothi7@gmail.com>
2026-02-02 21:20:42 +0530

    make a Admindashboard and working on Alltask file

---
+++ employee_management_system/src/Utils/localstorage.jsx
@@ -0,0 +1,20 @@
+{
+    const employee = [
+        { "email": "user1@example.com", "password": "1234" },
+        { "email": "user2@example.com", "password": "1234" },
+        { "email": "user3@example.com", "password": "1234" },
+        { "email": "user4@example.com", "password": "1234" },
+        { "email": "user5@example.com", "password": "1234" },
+        { "email": "user6@example.com", "password": "1234" },
+        { "email": "user7@example.com", "password": "1234" },
+        { "email": "user8@example.com", "password": "1234" },
+        { "email": "user9@example.com", "password": "1234" },
+        { "email": "user10@example.com", "password": "1234" }
+    ]
+}
+
+
+const admin = [{
+    "email": "admin@example.com",
+    "password": "1234"
+}]
